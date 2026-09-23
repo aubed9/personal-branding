4d07435c207ea5335d43422fe4d3ed3285a70125
Mohammad Aubed Isfahanizadeh <71547236+aubed9@users.noreply.github.com>
2026-09-24 01:02:00 +0330

    Export exact-domain Decision Modules

--- platform/src/reasoning/modules/index.js
+++ platform/src/reasoning/modules/index.js
@@ -2,4 +2,5 @@ export * from './axisPhaseEffects.js';
 export * from './contract.js';
 export * from './contextAdapter.js';
 export * from './registry.js';
+export * from './domainRegistry.js';
 export * from './composer.js';
